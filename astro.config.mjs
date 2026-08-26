957562e66dffdbdcb87bc651e0c3b90698a45c38
NastyBaster <rsn007@gmail.com>
2026-08-26 21:08:56 +0300

    Depoy at gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com',
-  output: 'static',
-  integrations: [sitemap()],
+  site: 'https://NastyBaster.github.io',
+  base: '/english-with-larisa',
 });
